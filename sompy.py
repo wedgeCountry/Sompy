828519e6af3506d1355fc40a4a539d67a5729c32
wedgeCountry <halbig.roland@freenet.de>
2014-02-15 13:43:16 +0100

    clean-up. !!! Renaming funtions and function calls !!!

--- sompy.py
+++ sompy.py
@@ -25,6 +25,7 @@ from math import *
 import numpy as np
 from PIL import Image
 import scipy.ndimage
+
 ########################################################################################################
 ## distance measures
 def eucl_distance(a, b):
@@ -42,74 +43,143 @@ def getBoundingBox(samples):
 		minmax[i,1] = max(samples[:,i])
 	return minmax
 
+########################################################################################################
+## SOM class
 class SOM():
-	''' Self Organizing Map Class. This implementation uses a rectengular grid. Other functionalities, such as
-	a counting array for the choices as Best Matching Unit and for the removal of Nodes are implemented. '''
-	def __init__(self, height=10, width=10, FV_size=10, learning_rate=0.005, initial_radius=None, FV_ranges='random'):
-		self.height = height
-		self.width = width
+	def __init__(self, height=10, width=10, FV_size=10, learning_rate=0.005, initial_radius=None, init_mode='random'):
+		''' 
+		Self Organizing Map. This implementation uses a rectengular grid. 
+		Additional functionalities, such as a counting array for the choices as Best Matching Unit and for the removal of Nodes are implemented. 
+		init_mode - values:
+			'random' : random vectors with values within the bounding box of the data
+			'xy_box': regular grid on the x-y plane. This functionality should be used on PCA-transformed data! 
+			'1d_line': regular line on the x-y plane. 
+		'''
+		self.height self.width, self.FV_size, self.learning_rate, self.init_mode = height, width, FV_size, learning_rate, init_mode
 		self.total = self.width * self.height
-		self.FV_size = FV_size
-		self.FV_ranges = FV_ranges
-		self.learning_rate = learning_rate
 		self.radius = (height+width)/3 if initial_radius is None else initial_radius
 
 		self.nodes = [[]] 				# saves node data
-		self.nodeDict = [[]] 				# saves index inside kohonen map
+		self.indexMap = [[]] 			# saves index inside kohonen map
 		self.BMUcount = [0]*self.total 	# counts bmu selection for each node
-		self.nodeIndex = [] 			# saves index of nodes in order to be able to add or remove nodes
+		self.nodeIndex = [] 			# saves index of nodes. Useful for adding or removing nodes
 	
-	## node access method for a rectangular grid
-	def getIndex(self, i, j):
-		return j + i*self.width
-	
-	def train(self, train_vector=[[]], iterations=100, num_samples = 1, continue_training=False):
+	def train(self, train_vector=[[]], iterations=100, num_samples = 1, cont=False, res=False, disp=True):
+		'''
+		train_vector - training data
+		iterations 		- number of iterations
+		cont - continue training
+		res  - calculate residuum, slow!
+		disp - display mode. True / False
+		'''
 		train_vector = np.asarray(train_vector)
 		max_iterations = int(iterations)
-		if not continue_training: 
-			self.initializeNodes(train_vector, self.FV_ranges)	
+		res_list = []
+		if not cont: 
+			self._initializeNodes(train_vector, self.init_mode)	
 		
 		for iteration in range(1, max_iterations+1):
-			sys.stdout.write("\rTraining Iteration: %d/%d" %(iteration, max_iterations)); 
-			sys.stdout.flush()
+			if disp:
+				sys.stdout.write("\rTraining Iteration: %d/%d" %(iteration, max_iterations)); 
+				sys.stdout.flush()
 			
-			current_radius = self.getRadius(iteration, max_iterations)
-			current_rate = self.getLearningRate(iteration, max_iterations)
-	
+			current_radius = self._getRadius(iteration, max_iterations)
+			current_rate = self._getLearningRate(iteration, max_iterations)
+			
+			if res: 
+				r = self._getResidual(train_vector, iteration, max_iterations)
+				res_list.append(r)
+				if iteration > max_iterations/4 and np.abs(res_list[-1]-res_list[-2]) < 0.1:
+					break;
+			
 			for j in range(num_samples):
 				j = int( np.random.uniform(0, len(train_vector),1))
-				
-				best = self.best_match(train_vector[j])				
-				for [neighbor, dist] in self.getNeighborhood(best, current_radius):
-					influence = self.getKernel(dist, current_radius, iteration)
-					self.nodes[neighbor,:] += influence * current_rate * (train_vector[j][:] - self.nodes[neighbor,:])
+				best = self._getBestMatch(train_vector[j])				
+				for [neighbor, dist] in self._getNeighborhood(best, current_radius):
+					influence = self._getKernel(dist, current_radius, iteration)
+					self.nodes[neighbor,:] += current_rate * influence * (train_vector[j][:] - self.nodes[neighbor,:])
 
-		sys.stdout.write("\n")
-		return;
+		if disp: sys.stdout.write("\n")
+		return res_list;
+
+	def getPrototypes(self):
+		''' 
+		Returns the grid containing the prototype vectors 
+		'''
+		return self._getDataInsideGrid(self.nodes)
+	
+	def saveSimilarityMask(self, filename, threshold=0, path="./"):
+		'''
+		Write the similarity mask to a file.
+		threshold - does thresholding for values from 0 to 255
+		'''
+		tmp_nodes = self._getDistanceMask()
+		self._saveMask(tmp_nodes, filename, path, threshold=threshold)
+	
+	def removeBmuNodes(self, threshold): 
+		''' 
+		You may want to do this before getting the data. 
+		This removes entries from the list of nodes with BMU count smaller than threshold. 
+		'''
+		for n in dict(self.nodeIndex):
+			index = self.nodeIndex[n]
+			if self.BMUcount[index] < threshold:
+				self.nodeIndex.pop(index)
+	
+	def saveBmuMask(self, filename, path="./"):
+		'''
+		Write the BMU mask to a file.
+		The ligther the value the more often a node has been chosen as Best Matching Unit.
+		'''
+		tmp_nodes = np.asarray(self.BMUcount)
+		self._saveMask(tmp_nodes, filename, path)
 		
+	def getSampledData(self):
+		'''
+		Returns the following data structures:
+		nodes	- list of the prototypes
+		idcs		- list of the corresponding SOM index
+		bmus		- list of the corresponding BMU counts
+		'''
+		idcs, nodes, bmus = [], [], []
+		for i in self.nodeIndex:
+			idcs.append(self.indexMap[i])
+			nodes.append(self.nodes[self.nodeIndex[i]])
+			bmus.append(self.BMUcount[self.nodeIndex[i]])
+		idcs = np.asarray(idcs)
+		nodes = np.asarray(nodes)
+		bmus = np.asarray(bmus)
+		return nodes, idcs, bmus
+	
+	###################################
+	###### private functions
+	
+	## node access method for a rectangular grid
+	def _getIndex(self, i, j):
+		return j + i*self.width
+
 	## parameter functions
-	def getKernel(self, distance, radius, iteration):
+	def _getKernel(self, distance, radius, iteration):
 		return exp( -1.0 * abs(distance) / (2*radius*iteration) )		
 
-	def getRadius(self, iteration, max_iterations):
+	def _getRadius(self, iteration, max_iterations):
 		return self.radius * exp(-1.0*log(self.radius+1)*(iteration-1)/max_iterations) 
 
-	def getLearningRate(self, iteration, max_iterations):
+	def _getLearningRate(self, iteration, max_iterations):
+		return self.learning_rate
 		return self.learning_rate * exp(-1.0*(iteration-1)/max_iterations*log(self.learning_rate+1))
 
-	# Returns location of best match, uses Euclidean distance
-	def best_match(self, target_FV): 
+	def _getBestMatch(self, target_FV): '''Returns location of best match, uses Euclidean distance '''
 		best = np.argmin(L1_distance(self.nodes, target_FV))
-		self.raiseBmuCount(best)
+		self._raiseBmuCount(best)
 		return best
 		
-	# Returns a list of points which live within 'dist' of 'pt'
-	# Uses the Chessboard distance; pt is (row, column)
-	def getNeighborhood(self, index, radius):
+	def _getNeighborhood(self, index, radius): ''' Returns a list of points which live within chessboard distance 'dist' of 'pt'. pt is (row, column). '''
 		if radius < 1: 
 			return [[index, 0.0]]
-		radius = int(radius)
-		pt = self.nodeDict[index]
+		else:
+			radius = int(radius)
+		pt = self.indexMap[index]
 		min_y = max(int(pt[0] - radius), 0)
 		max_y = min(int(pt[0] + radius + 1), self.height)
 		min_x = max(int(pt[1] - radius), 0)
@@ -121,20 +191,18 @@ class SOM():
 				#dist = (abs(y-pt[0])**2 + abs(x-pt[1])**2)**0.5
 				'''euclidean distance'''
 				dist = np.sqrt(abs(y-pt[0])**2 + abs(x-pt[1])**2)
-				neighbors.append([ self.getIndex(y,x), dist ])
+				neighbors.append([ self._getIndex(y,x), dist ])
 		return neighbors
 		
-	## initializes nodes as numpy arrays using FV_ranges					
-	def initializeNodes(self, train_vector=[[]], mode='random'):
+	def _initializeNodes(self, train_vector, mode='random'): ''' initializes nodes as numpy arrays using init_mode '''
 		self.nodes = np.asarray([ [0.0]*self.FV_size] * self.total)
-		self.nodeDict = [[0,0]]*self.total
+		self.indexMap = [[0,0]]*self.total
 		self.nodeIndex = dict()
 		for i in range(self.height):
 			for j in range(self.width):
-				index = self.getIndex(i,j)
-				self.nodeDict[index] = [i, j]	
+				index = self._getIndex(i,j)
+				self.indexMap[index] = [i, j]	
 				self.nodeIndex[index] = index		
-		
 		if mode == 'random':
 			''' random vectors with values within the bounding box of the data '''
 			minmax = getBoundingBox(train_vector)
@@ -147,21 +215,21 @@ class SOM():
 			hw = 1.0/float(self.width-1) if (self.width > 1) else 1
 			for i in range(self.height):
 				for j in range(self.width):
-					index = self.getIndex(i,j)
+					index = self._getIndex(i,j)
 					self.nodes[index, 0] = minmax[0,0] + i*hh*abs(minmax[0,1]-minmax[0,0])
 					self.nodes[index, 1] = minmax[1,0] + j*hw*abs(minmax[1,1]-minmax[1,0])			
 		elif mode == '1d_line':
 			''' regular line on the x-y plane. '''
 			if self.width > 1 and self.height > 1:
-				print 'Warning: In initializeNodes: cannot initialize 2D array as 1D line! Using random values!\n'
-				self.initializeNodes(train_vector, 'random')
+				print 'Warning: In _initializeNodes: cannot initialize 2D array as 1D line! Using random values!\n'
+				self._initializeNodes(train_vector, 'random')
 				return
 			self.nodes = np.asarray([ [0.0]*self.FV_size] * self.total)
 			minmax = getBoundingBox(train_vector)
 			dimension = self.total#max(self.height, self.width)
 			h = 1.0/float(dimension-1) if (dimension > 1) else 1
 			for i in range(dimension):
-					index = self.getIndex(i,0) if self.width < self.height else self.getIndex(0,i)
+					index = self._getIndex(i,0) if self.width < self.height else self._getIndex(0,i)
 					self.nodes[index,:] = minmax[:,0] + i*h*np.abs(minmax[:,0]-minmax[:,1])
 		elif len(mode) == 1:
 			''' random vectors with values within the range of [[min, max]] '''
@@ -170,104 +238,89 @@ class SOM():
 		else:
 			raise NotImplementedError( "ERROR! in SOM: Initialization mode not known!" )
 		return;
-				
-	## obtain the relevant som results 
-	def getSampledData(self):
-		idcs, nodes, bmus = [], [], []
-		for i in self.nodeIndex:
-			idcs.append(self.nodeDict[i])
-			nodes.append(self.nodes[self.nodeIndex[i]])
-			bmus.append(self.BMUcount[self.nodeIndex[i]])
-		idcs = np.asarray(idcs)
-		nodes = np.asarray(nodes)
-		bmus = np.asarray(bmus)
-		return idcs, nodes, bmus
 	
-	## get the 2D representation of the data 
-	def getDataInsideGrid(self, data):
-		transformedData = np.zeros((self.height, self.width), float)
+	def _getDataInsideGrid(self, data): '''Return 2D representation of the data. '''
+		N = np.zeros((self.height, self.width), float)
 		for index in range(self.total):
-			(i, j) = self.nodeDict[index]
-			transformedData[i,j] = data[index]
+			(i, j) = self.indexMap[index]
+			N[i,j] = data[index]
+		return N
 	
-	## obtain the relevant som results 
-	def getPrototypes(self):
-		return self.getDataInsideGrid(self.nodes)
+	def _getResidual(self, samples, iteration, max_iterations):
+		res = 0.0
+		radius = self._getRadius(iteration, max_iterations)
+		rate = self._getLearningRate(iteration, max_iterations)
+		for j in range(len(samples)):
+			best = self._getBestMatch(samples[j])			
+			for [neighbor, dist] in self._getNeighborhood(best, radius):
+				influence = self._getKernel(dist, radius, iteration)
+				res += rate * influence * eucl_distance(samples[j][:], self.nodes[neighbor,:])
+#			res += eucl_distance(samples[j][:], self.nodes[best,:])
+		return res
+	
 		
 	####################################################
 	## methods for counting the Best Matching Unit Index
-	def resetBmuCount(self):
+	def _resetBmuCount(self):
 		map( lambda x : 0, self.BMUcount )
 		
-	def raiseBmuCount(self, index):
+	def _raiseBmuCount(self, index):
 		self.BMUcount[index] += 1
-	
-	def getBmuValues(self):
-		return self.getDataInsideGrid(self.BMUcount)
+
+	def _getBmuValues(self):
+		return self._getDataInsideGrid(self.BMUcount)
 		
-	# do this before getting Sampled Data! This removes just from the dictionary
-	def removeBmuNodes(self, threshold):
-		for n in dict(self.nodeIndex):
-			index = self.nodeIndex[n]
-			if self.BMUcount[index] < threshold:
-				self.nodeIndex.pop(index)
-	
-	def save_BMU_mask(self, filename, path="./"):
-		tmp_nodes = np.asarray(self.BMUcount)
-		self.save_mask(tmp_nodes, filename, path)
-	
 	####################################################
 	## The SOM's similarity mask 
 	## which is some kind of average gradient. 
 	## The darker the area the more rapid the change.
 		
-	def build_distance_mask(self):
+	def _getDistanceMask(self):
 		tmp_nodes = np.zeros((self.total,), float)
 		for r in range(self.height):
 			for c in range(self.width):
-				index = self.getIndex(r,c)
+				index = self._getIndex(r,c)
 				N = self.nodes[index,:]
-				neighborhood = self.getNeighborhood(index, 1)
+				neighborhood = self._getNeighborhood(index, 1)
 				for [n, dist] in neighborhood:
-					tmp_nodes[n] += eucl_distance(self.nodes[index,:], N)
-				tmp_nodes[self.getIndex(r,c)] = tmp_nodes[self.getIndex(r,c)]**0.01
+					tmp_nodes[n] += eucl_distance(self.nodes[n,:], N)
+				tmp_nodes[self._getIndex(r,c)] = tmp_nodes[self._getIndex(r,c)]**0.01
 		return tmp_nodes
 		
-	def getSimilarityMask(self):
-		tmp_nodes = self.build_distance_mask()
-		return self.getDataInsideGrid(tmp_nodes)
-	
-	def save_similarity_mask(self, filename, path="./"):
-		tmp_nodes = self.build_distance_mask()
-		self.save_mask(tmp_nodes, filename, path)
-	
-	# save grid values to greyscale image
-	def save_mask(self, tmp_nodes, filename, path="./"):
+# save grid values to greyscale image
+	def _getSimilarityMask(self):
+		tmp_nodes = self._getDistanceMask()
+		return self._getDataInsideGrid(tmp_nodes)
+		
+	def _saveMask(self, tmp_nodes, filename, path="./", threshold=0):
 		tmp_nodes -= tmp_nodes.min()
 		tmp_nodes *= 255 / max(1, tmp_nodes.max())
 		tmp_nodes = 255 - tmp_nodes
+		if threshold > 0:
+			for i in range(len(tmp_nodes)):
+				tmp_nodes[i] = 0 if tmp_nodes[i] < threshold else 255
 		img = Image.new("L", (self.height,self.width))
 		for r in range(self.height):
 			for c in range(self.width):
 				#print r, c, self.width, self.height
 				img.putpixel((r,c), 0)
 		for i in self.nodeIndex:
-			img.putpixel(self.nodeDict[i], tmp_nodes[i])
+			value = tmp_nodes[i]
+			img.putpixel(self.indexMap[i], value)
 		scale = 20
 #		img = img.resize((self.width*int(scale),self.height*int(scale)),Image.NEAREST)
-		img.save(path + filename + ".png")
+		img.save(path + filename + ".pgm")
 	
 ################################################################################################
-
 
 class Torus_SOM(SOM):
 	''' This SOM uses a rectengular grid projected on the surface of a torus.
 		This piece of code is originally by kyle dickerson '''
-	def getNeighborhood(self, index, radius):
+	def _getNeighborhood(self, index, radius):
 		# returns a chessboard distance neighborhood, with distances determined by Euclidean distance
 		# - Meaning, take a square around the center pt
 		radius = int(radius)
-		pt = self.nodeDict[index]
+		pt = self.indexMap[index]
 		# This allows the grid to wrap vertically and horizontally
 		min_y = int(pt[0] - radius)
 		max_y = int(pt[0] + radius)+1
@@ -291,20 +344,17 @@ class Torus_SOM(SOM):
 				# Euclidean
 				d = (y_piece + (x-pt[1])**2)**0.5
 				x = x + self.width if x < 0 else x % self.width
-				neighbors.append([ self.getIndex(y,x), d ])
+				neighbors.append([ self._getIndex(y,x), d ])
 		return neighbors
 
 		
-class Linear_SOM(SOM):
+class Linear_SOM(Torus_SOM):
 		
 	# get decaying radius	
-	def getRadius(self, iteration, max_iterations):
+	def _getRadius(self, iteration, max_iterations):
 		return 1+self.radius * ((max_iterations - iteration) / max_iterations)
 	# get learning rate for each iteration
-	def getLearningRate(self, iteration, max_iterations):
+	def _getLearningRate(self, iteration, max_iterations):
 		return self.learning_rate * ((max_iterations - iteration) / max_iterations)
-	# get adaption coefficient
-	def getInfluence(self, distance, radius, iteration):
-		return (-distance + radius) / (radius)
-
+	
 
--- somtest.py
+++ somtest.py
@@ -10,13 +10,15 @@ if __name__ == "__main__":
 	t0 = time.time()
 	width = 20
 	height = 20
-	iterations = 100
-	color_som = SOM(width=width,height=height,FV_size=np.shape(colors)[1],learning_rate=0.5, FV_ranges='xy_box') 
+	iterations = 400
+	color_som = SOM(width=width,height=height,FV_size=np.shape(colors)[1],learning_rate=1.0, FV_ranges='xy_box') 
 	
 	print "Training colors..."
-	color_som.train(iterations=iterations, train_vector=colors, num_samples = len(colors))
-	color_som.save_similarity_mask("test_sim")
+	r = color_som.train(iterations=iterations, train_vector=colors, num_samples = 2+0*len(colors), residual = True)
 	
+	color_som.save_similarity_mask("test_sim")
+	if len(r) > 1:
+		print 'Residual:', r[-1]
 	t1 = time.time()
 	print 'time: %0.2f seconds' %(t1-t0)
 	
@@ -33,4 +35,17 @@ if __name__ == "__main__":
 		img.save("sompy_test_colors.png")
 	except:
 		print "Error saving the image, do you have PIL (Python Imaging Library) installed?"
-		
+	print "Saving Image: sompy_original_colors.png..."	
+	colors = np.asarray(colors)
+	img = Image.new("RGB", (4, 2))
+	img.putpixel((0,0),  (0, 0, 0))
+	img.putpixel((1,0),  (255, 255, 255))
+	img.putpixel((2,0),  (0, 255, 0))
+	img.putpixel((3,0),  (0, 255, 255))
+	img.putpixel((0,1), (255, 0, 0))
+	img.putpixel((1,1), (255, 0, 255))
+	img.putpixel((2,1), (255, 255, 0))
+	img.putpixel((3,1),  (0, 0, 255))
+	img = img.resize((width*10, height*10),Image.NEAREST)
+	img.save("sompy_original_colors.png")
+	
